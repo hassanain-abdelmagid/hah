83fd675170e08fbf310d4ba5f2a33ff00a1a695d
hassanain-abdelmagid <53225879+hassanain-abdelmagid@users.noreply.github.com>
2024-04-29 11:49:50 +0300

    Update gcp-webAPPdev.sh

--- gcp-webAPPdev.sh
+++ gcp-webAPPdev.sh
@@ -2,178 +2,46 @@
 
 
 
-gcloud config set compute/region $REGION
-export PROJECT_ID=$(gcloud config get-value project)
 
+gsutil mb gs://$DEVSHELL_PROJECT_ID
 
-gcloud services enable \
-  artifactregistry.googleapis.com \
-  cloudfunctions.googleapis.com \
-  cloudbuild.googleapis.com \
-  eventarc.googleapis.com \
-  run.googleapis.com \
-  logging.googleapis.com \
-  pubsub.googleapis.com
+bq mk $DATASET_NAME
 
-
-
-gsutil mb -l $REGION gs://$BUCKET_NAME
-
+bq mk --table \
+$DEVSHEL_PROJECT_ID:$DATASET_NAME.$TABLE_NAME \
+data:string
 
 gcloud pubsub topics create $TOPIC_NAME
 
+gcloud pubsub subscriptions create $TOPIC_NAME-sub --topic=$TOPIC_NAME
+
+gcloud dataflow flex-template run $JOB_NAME --template-file-gcs-location gs://dataflow-templates-$REGION/latest/flex/PubSub_to_BigQuery_Flex --region $REGION --temp-location gs://$DEVSHELL_PROJECT_ID/temp/ --parameters outputTableSpec=$DEVSHELL_PROJECT_ID:$DATASET_NAME.$TABLE_NAME,inputTopic=projects/$DEVSHELL_PROJECT_ID/topics/$TOPIC_NAME,outputDeadletterTable=$DEVSHELL_PROJECT_ID:$DATASET_NAME.$TABLE_NAME,javascriptTextTransformReloadIntervalMinutes=0,useStorageWriteApi=false,useStorageWriteApiAtLeastOnce=false,numStorageWriteApiStreams=0
 
 
-PROJECT_NUMBER=$(gcloud projects list --filter="project_id:$PROJECT_ID" --format='value(project_number)')
-SERVICE_ACCOUNT=$(gsutil kms serviceaccount -p $PROJECT_NUMBER)
-
-gcloud projects add-iam-policy-binding $PROJECT_ID \
-  --member serviceAccount:$SERVICE_ACCOUNT \
-  --role roles/pubsub.publisher
-  
-
-mkdir ~/quicklab && cd $_
-touch index.js && touch package.json
-
-
-cat > index.js <<'EOF_END'
-const functions = require('@google-cloud/functions-framework');
-const crc32 = require("fast-crc32c");
-const { Storage } = require('@google-cloud/storage');
-const gcs = new Storage();
-const { PubSub } = require('@google-cloud/pubsub');
-const imagemagick = require("imagemagick-stream");
-
-functions.cloudEvent('memories-thumbnail-maker', cloudEvent => {
-  const event = cloudEvent.data;
-
-  console.log(`Event: ${event}`);
-  console.log(`Hello ${event.bucket}`);
-
-  const fileName = event.name;
-  const bucketName = event.bucket;
-  const size = "64x64"
-  const bucket = gcs.bucket(bucketName);
-  const topicName = "memories-topic-374";
-  const pubsub = new PubSub();
-  if ( fileName.search("64x64_thumbnail") == -1 ){
-    // doesn't have a thumbnail, get the filename extension
-    var filename_split = fileName.split('.');
-    var filename_ext = filename_split[filename_split.length - 1];
-    var filename_without_ext = fileName.substring(0, fileName.length - filename_ext.length );
-    if (filename_ext.toLowerCase() == 'png' || filename_ext.toLowerCase() == 'jpg'){
-      // only support png and jpg at this point
-      console.log(`Processing Original: gs://${bucketName}/${fileName}`);
-      const gcsObject = bucket.file(fileName);
-      let newFilename = filename_without_ext + size + '_thumbnail.' + filename_ext;
-      let gcsNewObject = bucket.file(newFilename);
-      let srcStream = gcsObject.createReadStream();
-      let dstStream = gcsNewObject.createWriteStream();
-      let resize = imagemagick().resize(size).quality(90);
-      srcStream.pipe(resize).pipe(dstStream);
-      return new Promise((resolve, reject) => {
-        dstStream
-          .on("error", (err) => {
-            console.log(`Error: ${err}`);
-            reject(err);
-          })
-          .on("finish", () => {
-            console.log(`Success: ${fileName} → ${newFilename}`);
-              // set the content-type
-              gcsNewObject.setMetadata(
-              {
-                contentType: 'image/'+ filename_ext.toLowerCase()
-              }, function(err, apiResponse) {});
-              pubsub
-                .topic(topicName)
-                .publisher()
-                .publish(Buffer.from(newFilename))
-                .then(messageId => {
-                  console.log(`Message ${messageId} published.`);
-                })
-                .catch(err => {
-                  console.error('ERROR:', err);
-                });
-          });
-      });
-    }
-    else {
-      console.log(`gs://${bucketName}/${fileName} is not an image I can handle`);
-    }
-  }
-  else {
-    console.log(`gs://${bucketName}/${fileName} already has a thumbnail`);
-  }
-});
-
-EOF_END
-
-
-sed -i "8c\functions.cloudEvent('$FUNCTION_NAME', cloudEvent => {" index.js
-
-sed -i "18c\  const topicName = '$TOPIC_NAME';" index.js
-
-
-
-cat > package.json <<EOF_END
-{
-    "name": "thumbnails",
-    "version": "1.0.0",
-    "description": "Create Thumbnail of uploaded image",
-    "scripts": {
-      "start": "node index.js"
-    },
-    "dependencies": {
-      "@google-cloud/functions-framework": "^3.0.0",
-      "@google-cloud/pubsub": "^2.0.0",
-      "@google-cloud/storage": "^5.0.0",
-      "fast-crc32c": "1.0.4",
-      "imagemagick-stream": "4.1.1"
-    },
-    "devDependencies": {},
-    "engines": {
-      "node": ">=4.3.2"
-    }
-  }
-EOF_END
-
-
-
-deploy_function () {
-gcloud functions deploy $FUNCTION_NAME \
-  --gen2 \
-  --runtime nodejs20 \
-  --entry-point $FUNCTION_NAME \
-  --source . \
-  --region $REGION \
-  --trigger-bucket $BUCKET_NAME \
-  --trigger-location $REGION \
-  --max-instances 1 \
-  --quiet
-}
-
-# Variables
-SERVICE_NAME="$FUNCTION_NAME"
-
-# Loop until the Cloud Run service is created
 while true; do
-  # Run the deployment command
-  deploy_function
+    STATUS=$(gcloud dataflow jobs list --region="$REGION" --format='value(STATE)' | grep Running)
+    
+    if [ "$STATUS" == "Running" ]; then
+        # Run the next code here
+        echo "The Dataflow job is running. Proceeding with the next code."
+        # Add your next code here
 
-  # Check if Cloud Run service is created
-  if gcloud run services describe $SERVICE_NAME --region $REGION &> /dev/null; then
-    echo "Cloud Run service is created. Exiting the loop."
-    break
-  else
-    echo "Waiting for Cloud Run service to be created..."
-    sleep 10
-  fi
+        sleep 20
+        gcloud pubsub topics publish $TOPIC_NAME --message='{"data": "73.4 F"}'
+
+        bq query --nouse_legacy_sql "SELECT * FROM \`$DEVSHELL_PROJECT_ID.$DATASET_NAME.$TABLE_NAME\`"
+        break  # exit the loop since the job is running
+    else
+        # Wait for a certain amount of time before checking again
+        sleep 30  # you can adjust the sleep duration based on your needs
+        echo "The Dataflow job is not running. Waiting for it to start..."
+        # Continue to the next iteration of the loop
+    fi
 done
 
 
-wget https://storage.googleapis.com/cloud-training/gsp315/map.jpg 
-
-gsutil cp map.jpg gs://$BUCKET_NAME
 
 
-1
+gcloud pubsub topics publish $TOPIC_NAME --message='{"data": "73.4 F"}'
+
+bq query --nouse_legacy_sql "SELECT * FROM \`$DEVSHELL_PROJECT_ID.$DATASET_NAME.$TABLE_NAME\`"
